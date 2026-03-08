8dc477acd939a7056ab0fe76ea6c2868fab3ffa0
Francisco Peebles <fpeebles@franciscopeebles.com>
2026-03-08 13:05:58 -0500

    Rebuild app.js after merge (hostedmode)

--- public/app.js
+++ public/app.js
@@ -23231,7 +23231,7 @@ ${beacon.location}`);
   init_dom();
   function initUpdateDisplay() {
     const el2 = $("platformLabel");
-    if (el2) el2.textContent = "v0.64.0";
+    if (el2) el2.textContent = "v0.64.1";
   }
 
   // src/settings-sync.js
